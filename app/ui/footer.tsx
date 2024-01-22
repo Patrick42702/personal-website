b20853c316458cd4395b917c18f54cd79f1fc6e4
Patrick Muller <patrick.muller.1@stonybrook.edu>
2024-01-22 13:36:59 -0500

    formatted file

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,6 +1,6 @@
-export function Footer(){
-    return(
-    <footer className="bg-[#041B24] h-32 w-full">        
-    </footer>
+export function Footer() {
+    return (
+        <footer className="bg-[#041B24] h-32 w-full">
+        </footer>
     )
 }
