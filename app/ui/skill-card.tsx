90aaad3caa35488029e124285dd4da72d0438ad9
Patrick Muller <patrick.muller.1@stonybrook.edu>
2024-01-30 13:52:50 -0500

    fixed mapping

--- app/ui/skill-card.tsx
+++ app/ui/skill-card.tsx
@@ -1,74 +1,37 @@
 "use client"
 import Image from 'next/image'
-
+import { CARD_INFO } from '@/utils/utils'
 
 export function CardWrapper(){
-    const python_text = "I have experience coding in python on projects such as ___,___,___"
-    const java_text = "I have experience coding in java on projects such as ___,___,___"
-    const c_text = "I have experience coding in C on projects such as ___,___,___"
-    const web_skills_text = "I have experience coding in html/css/js on projects such as ___,___,___"
-    const ocaml_text = "I have experience coding in oCaml"
-    const mongodb_text = "I have experience working with mongoDb"
-    const github_text = "I have experience working with github for version control for many projects"
-    const linux_text = "I have experience working with linux"
-    const node_text = "I have experience working with node.js"
     
     return(
-        <section className='flex flex-wrap gap-10 justify-between min-h-full py-8'>
-            <Card lang={"Python"} text={`${python_text}`}
-            width={80} 
-            imgs={[["Python-logo.svg", "python image"]]}
-            />
-            <Card lang={"Java"} text={`${java_text}`}
-            imgs={[["java-4.svg","python image"]]}
-            width={80} 
-            />
-            <Card lang={"C"} text={`${c_text}`}
-            imgs={[["c-1.svg","python image"]]}
-            width={80} 
-            />
-            <Card lang={"front-end stack"} text={`${web_skills_text}`}
-            imgs={[["html-1.svg","python image"],
-                    ["css-3.svg", "css image"],
-                    ["logo-javascript.svg", "javascript image"] ]}
-            width={80} 
-            />
-            <Card
-            lang={"oCaml"} text={`${ocaml_text}`}
-            imgs={[["ocaml.svg","oCaml image"]]}
-            width={120}
-            />
-            <Card
-            lang={"Linux"} text={`${linux_text}`}
-            imgs={[["linux-icon.svg","linux image"]]}
-            width={120}
-            />
-            <Card
-            lang={"node.js"} text={`${node_text}`}
-            imgs={[["nodejs-3.svg","nodejs image"]]}
-            width={120}
-            />
+        <section className='flex flex-wrap gap-10 min-h-full py-8'>
+            {CARD_INFO.map(card => 
+                <Card
+                lang={card.lang}
+                text={card.text}
+                imgs={card.imgs}
+                width={80} 
+                />
+                )}
         </section>
     )
 }
 
 
 function Card({lang, text, width, imgs} : 
-    {lang: string, text:string, width: number, imgs: [string, string][]}) {
-        const frmt_imgs: JSX.Element[] = [];
-        imgs.forEach(img =>
-            frmt_imgs.push(
-                <Image
-                src={img[0]}
-                alt={img[1]}
-                width={width}
-                height={width}
-                />
-            ))
+    {lang: string, text:string, width: number, imgs: string[][]}) {        
     return (
         <div className='bg-cyan-900 w-72 h-96 rounded-lg px-2 py-4'>
             <div className='flex flex-wrap gap-2'>
-                {frmt_imgs}
+                {imgs.map(img =>
+                    <Image
+                        src={img[0]}
+                        alt={img[1]}
+                        width={width}
+                        height={width}
+                    />
+            )}
             </div>
             <p className='text-cyan-400 text-2xl mt-2'>{`${lang}`}</p>
             <p className='mt-4'>{`${text}`}</p>
---
+++ utils/utils.ts
@@ -0,0 +1,39 @@
+export const CARD_INFO = [
+    {
+        lang: "Python",
+        text: "I have experience coding in python on projects such as ___,___,___",
+        imgs: [["/Python-logo.svg, python image"]],
+    },
+    {
+        lang: "Java",
+        text: "I have experience coding in java on projects such as ___,___,___",
+        imgs: [["/java-4.svg","java image"]],
+    },
+    {
+        lang: "C",
+        text: "I have experience coding in C on projects such as ___,___,___",
+        imgs: [["/c-1.svg, C image"]],
+    },
+    {
+        lang: "Front-End Stack",
+        text: "I have experience coding in html/css/js on projects such as ___,___,___",
+        imgs: [["/html-1.svg","python image"],
+            ["/css-3.svg", "css image"],
+            ["/logo-javascript.svg", "javascript image"]]
+    },
+    {
+        lang: "oCaml",
+        text: "I have experience coding in oCaml",
+        imgs: [["/ocaml.svg","oCaml image"]],
+    }, 
+    {
+        lang: "Linux",
+        text: "I have experience working with linux",
+        imgs: [["/linux-icon.svg","linux image"]],
+    }, 
+    {
+        lang: "Node.js",
+        text: "I have experience working with node.js",
+        imgs: [["/nodejs-3.svg","nodejs image"]],
+    },
+]
